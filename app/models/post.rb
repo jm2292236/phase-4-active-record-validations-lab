31ea257d1fe1777a945f5b9e224c23dcd8032750
Jaime Montes de Oca <jaime0422@live.com>
2022-08-25 21:35:25 -0700

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, exclusion: {in: ["True Facts"]}
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :is_click_bait
+
+    # create a regular expression with the valid texts that need to be in the title
+    VALID_TEXT = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+    
+    def is_click_bait
+        if VALID_TEXT.none? { |pat| pat.match title }
+            errors.add(:title, "Title is not clickbait")
+        end
+    end
 end
